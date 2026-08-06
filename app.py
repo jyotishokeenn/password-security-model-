b185f0606b34820667e52785bbcf216ab09d70d7
jyotishokeenn <jyotishokeen789@gmail.com>
2026-08-06 10:41:28 +0530

    Increase image width from 350 to 500

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import google.generativeai as genai
 # PROJECT IMAGE 
 st.image(
     "https://humanfocus.co.uk/wp-content/uploads/password-security-800x800.jpg",
-    width=350
+    width=500
 )
 
 # Password Strength Checker
